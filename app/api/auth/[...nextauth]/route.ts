01efa6b44a27543764fcf5c5118e0b0112e2ed28
shubham <hoodashubham92@gmail.com>
2024-07-30 11:14:36 +0530

    added db adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,20 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+// import prisma from "@/prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
+  // Configure one or more authentication providers
   providers: [
     Google({
-      // ! - used for telling typescript compiler that we have a valid value for the variable
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOLE_CLIENT_SECRET!,
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 });
 
 export { handler as GET, handler as POST };
